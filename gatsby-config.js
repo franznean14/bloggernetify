5eb57e67a9bf3b25079a3164b0aef97fe5c24ac8
Franz Casabuena <franzneandra@gmail.com>
2019-12-21 13:51:10 +0800

    added filesystem assets to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/static/assets`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         excerpt_separator: `<!-- endexcerpt -->`,
