335f12f47e53e3fe6ff2c9e829326b10c6ec0914
Franz Casabuena <franzneandra@gmail.com>
2019-12-21 13:57:27 +0800

    configured filesystem for assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `assets`,
+        name: `images`,
         path: `${__dirname}/static/assets`,
       },
     },
